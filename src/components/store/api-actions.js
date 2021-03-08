aac36a5898f04c6bef828003a4b9f275bc3229e8
Albert <zubov.warrior@gmail.com>
2021-03-08 22:03:04 +0300

    Личный проект: истина где-то на сервере (в работе 2)

--- src/components/store/api-actions.js
+++ src/components/store/api-actions.js
@@ -1,4 +1,28 @@
-export const fetchPlacesList = () => (dispatch, _getState, api) =>
-  api.get(`/hotels`).then(({ data }) => dispatch());
+import { ActionCreator } from '../store/action';
+import { AuthorizationStatus } from '../../const/const';
 
-// TODO Остановился тут
+export const fetchPlacesList = () => (dispatch, _getState, api) =>
+  api
+    .get(`/hotels`)
+    .then(({ data }) => dispatch(ActionCreator.loadingHotels(data)));
+
+export const checkAuth = () => (dispatch, _getState, api) =>
+  api
+    .get(`/login`)
+    .then(() =>
+      dispatch(ActionCreator.requereAuthorization(AuthorizationStatus.AUTH))
+    )
+    .catch((err) => {
+      throw err;
+    });
+
+export const login = ({ login: email, password }) => (
+  dispatch,
+  _getState,
+  api
+) =>
+  api
+    .post(`/login`, { email, password })
+    .then(() =>
+      dispatch(ActionCreator.requereAuthorization(AuthorizationStatus.AUTH))
+    );
--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { createAPI } from './services/api';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { ActionCreator } from './components/store/action';
 import { AuthorizationStatus } from './const/const';
+import { fetchPlacesList, checkAuth } from './components/store/api-actions';
 
 const COUNT_CARD_OFFERS = 50;
 const COUNT_COMMENTS = 1;
@@ -18,6 +19,8 @@ const COUNT_CARD_OFFERS_NEARBY = 3;
 const arrOffers = createArrElements(COUNT_CARD_OFFERS, getOffer);
 const arrOffersNearby = createArrElements(COUNT_CARD_OFFERS_NEARBY, getOffer);
 const arrComments = createArrElements(COUNT_COMMENTS, getComment);
+
+// TODO удалить моковые офферы и прокинуть их напрямую в app из store
 
 const api = createAPI(() =>
   store.dispatch(
@@ -29,6 +32,9 @@ const store = createStore(
   reducer(arrOffers),
   composeWithDevTools(applyMiddleware(thunk.withExtraArgument(api)))
 );
+
+store.dispatch(fetchPlacesList());
+// store.dispatch(checkAuth());
 
 ReactDOM.render(
   <Provider store={store}>
